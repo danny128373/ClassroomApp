0d6de94853c54dd5bea79cc6384451a9f7846749
Daniel Meza <daniel.meza29@yahoo.com>
2020-08-08 21:56:46 -0500

    fix: Added url for admin

--- classroomproject/urls.py
+++ classroomproject/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('classroomapp.urls'))
+    path('admin/', admin.site.urls),
+    path('', include('classroomapp.urls')),
 ]
